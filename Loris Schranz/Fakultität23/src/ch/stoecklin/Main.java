f3c849e5652d802b563f19bb605d2a483044705f
Loscho2k17 <schranz.loris@gmail.com>
2021-11-03 14:29:21 +0100

    Working functionality, test aren't done yet.

--- Loris Schranz/Fakultität23/src/ch/stoecklin/Main.java
+++ Loris Schranz/Fakultität23/src/ch/stoecklin/Main.java
@@ -1,13 +1,20 @@
 package ch.stoecklin;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello World!");
+        Scanner input = new Scanner(System.in);
+        System.out.println(n23(input.nextInt()));
     }
 
-    public static void n23(int number){
-
+    public static long n23(int number){
+        long result = 1;
+        for (int i = 0; i<number; i++){
+            if(i%2!=0 && i%3!=0){
+                result *= i;
+            }
+        }
+        return result;
     }
 }

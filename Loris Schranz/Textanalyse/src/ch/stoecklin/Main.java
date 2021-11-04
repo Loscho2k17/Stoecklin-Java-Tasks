f1d9772e6f4e05e081071c58e5250bdf9fa941f4
Loscho2k17 <schranz.loris@gmail.com>
2021-11-04 13:07:44 +0100

    finishing both tasks in ca 90 minutes. (Took some time to learn some Regex expressions and wasn't familiar with Java 11 Files)

--- Loris Schranz/Textanalyse/src/ch/stoecklin/Main.java
+++ Loris Schranz/Textanalyse/src/ch/stoecklin/Main.java
@@ -1,9 +1,52 @@
 package ch.stoecklin;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello World!");
+    public static final HashMap<String, Integer> countWordsFromSentence = new HashMap<>();
+    public static final HashMap<String, Integer> countLettersFromSentence = new HashMap<>();
+
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        String test = "Dieser Satz dient nur als Test.";
+        String test2 = Files.readString(Path.of("src\\ch\\stoecklin\\resources\\text.txt"));
+
+        countWords(test);
+        countLetters(test);
+
+        System.out.println(countWordsFromSentence);
+        System.out.println(countLettersFromSentence);
+
+        countLettersFromSentence.clear();
+        countWordsFromSentence.clear();
+
+        countWords(test2);
+        countLetters(test2);
+
+        System.out.println(countWordsFromSentence);
+        System.out.println(countLettersFromSentence);
+    }
+
+    public static void countWords(String sentence) {
+        for (String word : List.of(sentence.split("[^(?!A-Za-z_äÄöÖüÜ)]"))) {
+            countWordsFromSentence.put(word, countWordsFromSentence.get(word) == null ? 1 : countWordsFromSentence.get(word) + 1);
+        }
+    }
+
+    public static void countLetters(String sentence) {
+        for (char letter : sentence.toCharArray()) {
+            if (!String.valueOf(letter).matches("[^A-Za-z_äÄöÖüÜ]")) {
+                countLettersFromSentence.put(String.valueOf(letter), countLettersFromSentence.get(String.valueOf(letter)) == null ? 1 : countLettersFromSentence.get(String.valueOf(letter)) + 1);
+            }
+        }
     }
 }

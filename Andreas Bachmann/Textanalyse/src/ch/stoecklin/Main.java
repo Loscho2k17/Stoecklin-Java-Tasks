6b3648804436cabd36c9fec359aca893ba39b89e
Andreas Bachmann <andreas.bachmann@ssw.local>
2021-11-15 10:52:01 +0100

    Adding Textfile reading.

--- Andreas Bachmann/Textanalyse/src/ch/stoecklin/Main.java
+++ Andreas Bachmann/Textanalyse/src/ch/stoecklin/Main.java
@@ -1,16 +1,22 @@
 package ch.stoecklin;
 
 
+
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		String eingabe = input.nextLine().toLowerCase();
-		String[] split  = eingabe.split(" ");
+	public static void main(String[] args)throws IOException {
+		//Scanner input = new Scanner(System.in);
+		//String eingabe = input.nextLine().toLowerCase();
+		String eingabe2 = Files.readString(Path.of("src\\ch\\stoecklin\\Testtext.txt"));
+		String[] split  = eingabe2.split(" ");
 		Map<String, Integer > test1 = new HashMap<String, Integer>();
 		for(String a: split) {
 		
@@ -18,6 +24,13 @@ public class Main {
 			test1.put(a,test1.get(a) == null ? 1 : test1.get(a) + 1);
 		}
 		test1.forEach((k,v) -> System.out.println(k + " (" + v + "), "));
+		
+		String[] split2 = eingabe2.split("");
+		Map<String, Integer> test2 = new HashMap<String, Integer>();
+		for(String a: split2) {
+			test2.put(a,test2.get(a) == null ? 1 : test2.get(a) + 1);
+		}
+		test2.forEach((k,v) -> System.out.println(k+" ("+ v +"),"));
 	}
 	
 
